ce8443ac0b78c62536304856d025667b74d17f20
rajan-poudel <rajan.rp50@gmail.com>
2022-11-30 15:40:54 +0545

    select  video form videos list and launcher logo

--- lib/domain/usecases/get_video.dart
+++ lib/domain/usecases/get_video.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:movie_app/domain/entities/app_error.dart';
-import 'package:movie_app/domain/entities/cast_entity.dart';
 import 'package:movie_app/domain/entities/movie_params.dart';
 import 'package:movie_app/domain/entities/watch_video_Entity.dart';
 import 'package:movie_app/domain/repositories/movie_repositories.dart';
@@ -11,7 +10,8 @@ class GetVideo extends UseCase<List<WatchVideoEntity>, MovieParams> {
   GetVideo(this.movieRepository);
 
   @override
-  Future<Either<AppError, List<WatchVideoEntity>>> call(MovieParams params) async {
+  Future<Either<AppError, List<WatchVideoEntity>>> call(
+      MovieParams params) async {
     return await movieRepository.getVideo(params.id);
   }
 }
--- lib/presentation/screen/watch_video/watch_video_screen.dart
+++ lib/presentation/screen/watch_video/watch_video_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/common/constants/size_constant.dart';
 import 'package:movie_app/common/constants/translation_constants.dart';
@@ -6,6 +7,8 @@ import 'package:movie_app/domain/entities/watch_video_Entity.dart';
 import 'package:movie_app/presentation/screen/app_localization.dart';
 import 'package:movie_app/presentation/screen/watch_video/watch_video_arguments.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+import '../../themes/theme_color.dart';
 
 class WatchVideoScreen extends StatefulWidget {
   final WatchVideoArgument watchVideoArgument;
@@ -24,14 +27,15 @@ class _WatchVideoScreenState extends State<WatchVideoScreen> {
   void initState() {
     _videos = widget.watchVideoArgument.videos;
     _controller = YoutubePlayerController(
-        initialVideoId: _videos[0].key,
-        flags: const YoutubePlayerFlags(
-          autoPlay: true,
-          mute: false,
-          // isLive: true,
-          enableCaption: true,
-          captionLanguage: "en",
-        ));
+      initialVideoId: _videos[0].key,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+        // isLive: true,
+        enableCaption: true,
+        captionLanguage: "en",
+      ),
+    );
     super.initState();
   }
 
@@ -45,6 +49,7 @@ class _WatchVideoScreenState extends State<WatchVideoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: AppColor.vulcan,
         title: Text(AppLocalizations.of(context)!
             .translate(TranslationConstants.watchTrailers)
             .toString()),
@@ -56,43 +61,55 @@ class _WatchVideoScreenState extends State<WatchVideoScreen> {
             children: [
               player,
               Expanded(
-                  child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    for (int i = 0; i < _videos.length; i++)
-                      SizedBox(
-                        height: getProportionateScreenHeight(60),
-                        child: Row(
-                          children: [
-                            GestureDetector(
-                              onTap: () {
-                                _controller.load(_videos[i].key);
-                                _controller.play();
-                              },
-                              child: Image.asset(
-                                YoutubePlayer.getThumbnail(
-                                  videoId: _videos[i].key,
-                                  quality: ThumbnailQuality.high,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      for (int i = 0; i < _videos.length; i++)
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: Sizes.dimen_10,
+                          ),
+                          height: getProportionateScreenHeight(60),
+                          child: Row(
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  _controller.load(_videos[i].key);
+                                  _controller.play();
+                                },
+                                child: CachedNetworkImage(
+                                  width: Sizes.dimen_150,
+                                  imageUrl: YoutubePlayer.getThumbnail(
+                                    videoId: _videos[i].key,
+                                    quality: ThumbnailQuality.high,
+                                  ),
                                 ),
                               ),
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: getProportionateScreenWidth(
-                                        Sizes.dimen_6)),
-                                child: Text(
-                                  _videos[i].name,
-                                  style: const TextStyle(color: Colors.white),
+                              Expanded(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: getProportionateScreenWidth(
+                                          Sizes.dimen_6)),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      _controller.load(_videos[i].key);
+                                      _controller.play();
+                                    },
+                                    child: Text(
+                                      _videos[i].name,
+                                      style:
+                                          const TextStyle(color: Colors.white),
+                                    ),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                  ],
+                    ],
+                  ),
                 ),
-              ))
+              )
             ],
           );
         },
